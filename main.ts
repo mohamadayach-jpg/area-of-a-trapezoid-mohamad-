e434e5eafcb22f5ed3dff23a9dfdf43743df9918
mohamadayach-jpg <mohamad.ayach@stu.ocsb.ca>
2026-05-08 14:00:21 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+// Created by :Mohamad Ayach
+// Created on :May 6,2026
+// This program calculates the area of a trapezoid by asking the user for the measures of base 1, base 2, and the height. It then calculates and displays the area of the trapezoid.
+// 
 game.splash("lets calculate the area of a trapezoid")
 let base_1 = game.askForNumber("what is base 1 (cm)")
 let base_2 = game.askForNumber("what is base 2 (cm)")
